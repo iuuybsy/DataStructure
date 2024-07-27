fdb21786c7cda83c57b7e3f66b8d5048227f42cd
mfdlshz <mfdlshz@163.com>
2024-07-27 14:53:55 +0800

    [ADD]Add doubly node.

--- LinearList/Node.h
+++ LinearList/Node.h
@@ -4,9 +4,20 @@
 template <typename T>
 class Node {
 public:
-    T data;
+    T val;
     T* next;
     Node(): next(nullptr) {};
+    Node(T val_): val(val_), next(nullptr) {};
+};
+
+template <typename T>
+class DoublyNode {
+public:
+    T val;
+    T* next;
+    T* previous;
+    DoublyNode(): next(nullptr), previous(nullptr) {};
+    DoublyNode(T val_): val(val_), next(nullptr), previous(nullptr) {};
 };
 
 #endif //DATASTRUCTURE_NODE_H
